0f8339cad892bc8177cdbc5c0f13c5cfdd51cb40
qa-vzshb-33-knds@example.com <qa-vzshb-33-knds@example.com>
2023-02-28 19:30:39 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15469.stoplight-dev.com/api/v1/projects/cHJqOjI0OQ/spectral.js?branch_slug=main&token=44613e0e-95a3-4c3c-8ae0-a3b198b6c197";
+export default { extends: ruleset };
